043379e073c9c822550f341146112acbc8966873
Inna Sarra Manova <n01384347@nj-00118-018.humber.org>
2020-02-03 14:16:05 -0500

    0302

--- server.js
+++ server.js
@@ -30,23 +30,23 @@ app.get("/products", (req, res) => {
 // listings
 
     fakeDB.push({
-        title: 'Island house', description: `Our rearest listing. Green house in the middle of the lake Hgtamik`, price: `256`, image: '/img/1.jpg'
+        title: 'Island house', description: `Our rearest listing. Green house in the middle of the lake Hgtamik`, price: `256`, image: '/img/1.jpg',beds: '2'
     });
 
     fakeDB.push({
-        title: 'Student unit by designer', description: `Perfect unit for the student who wishes studing get away`, price: `178`, image: '/img/2.jpg'
+        title: 'Student unit by designer', description: `Perfect unit for the student who wishes studing get away`, price: `178`, image: '/img/2.jpg',beds: '4'
     });
 
     fakeDB.push({
-        title: 'Family cabin', description: `Family cabin for a big-medium-small size family. Bring them all`, price: `165`, image: '/img/3.jpg'
+        title: 'Family cabin', description: `Family cabin for a big-medium-small size family. Bring them all`, price: `165`, image: '/img/3.jpg',beds: '6'
     });
 
     fakeDB.push({
-        title: 'Hobbit house 1', description: `Like "Lord of the rings?" This is a place for you`, price: `211`, image: '/img/4.jpg'
+        title: 'Hobbit house 1', description: `Like "Lord of the rings?" This is a place for you`, price: `211`, image: '/img/4.jpg',beds: '8'
     });
 
     fakeDB.push({
-        title: 'High and awesome', description: `Design house just in the heart of the city`, price: `298`, image: '/img/5.jpg',people: '6'
+        title: 'High and awesome', description: `Design house just in the heart of the city`, price: `298`, image: '/img/5.jpg',beds: '6'
     });
 
     res.render("products", {
