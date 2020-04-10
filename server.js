0622ce237787b215a3a7cc17afe2f1e9444378f9
@Inna <55448200+Inna-WD@users.noreply.github.com>
2020-04-10 17:43:31 -0400

    1004

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 require('dotenv').config({path:"./config/keys.env"});
 const app = express();
 
@@ -22,7 +23,7 @@ const featuredController = require("./controllers/featured");
 //map each controller to the app object
 
 
-const mongoose = require('mongoose');
+
 mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
 console.log(`Connected to MongoDB`);
