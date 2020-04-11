52bd6b501be87b7f497260f79a4e098fb1face6d
@Inna <55448200+Inna-WD@users.noreply.github.com>
2020-04-11 19:42:45 -0400

    1104

--- models/user.js
+++ models/user.js
@@ -1,39 +1,59 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-// schema for the users - documents
-const userSchema = new Schema({
-    firstName: {
-        type: String,
-        require: true
-    },
-    LastName: {
-        type: String,
-        require: true
-    },
-    phone: {
-        type: Number,
-        require: true
-    },
-    email: {
-        type: String,
-        require: true
-    },
-    password: {
-        type: String,
-        default: "New"
-    },
-    profilePic:{
-        type: String,
-        require: false
-    },
-    dateCreated: {
-        type: Date,
-        default: Date.now()
-    },
-    createdBy: {
+const bcrypt = require("bcryptjs");
 
+//This indicates the shape of the documents that will be entering the database
+  const userSchma = new Schema({
+
+    firstName:
+    {
+        type:String,
+        required:false
+    },
+
+    lastName:
+    {
+        type:String,
+        required:false
+    },
+
+    email:
+    {
+        type:String,
+        required:true
+    },
+    password:
+    {
+        type:String,
+        required:true
+    },
+    profilePic:
+    {
+        type:String,
+        required:false
+    },
+    dateCreated:
+    {
+        type:Date,
+        default:Date.now()
     }
-});
+  });
 
-const userModel = mongoose.model('User', userSchema);
-module.exports = userModel;
+
+
+userSchma.pre("save",function(next)
+{
+    bcrypt.genSalt(10)
+    .then((salt)=>{
+        bcrypt.hash(this.password,salt)
+        .then((encryptPassword)=>{
+            this.password = encryptPassword;
+            next();
+        })
+        .catch(err=>console.log(`Error occured ${err}`));
+    })
+    .catch(err=>console.log(`Error occured ${err}`));
+})
+ const userModel = mongoose.model('User', userSchma);
+
+ module.exports = userModel;
--- server.js
+++ server.js
@@ -2,18 +2,10 @@ const express = require("express");
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+// const session = require('express-session')
+
 require('dotenv').config({path:"./config/keys.env"});
-
-const app = express();
-
-//This allows express to make my static content available from the public
-app.use(express.static('public'));
-
-
-
-
-//parse application
-app.use(bodyParser.urlencoded({ extended: false }))
 
 //load controllers
 const generalController = require("./controllers/general");
@@ -22,35 +14,17 @@ const featuredController = require("./controllers/featured");
 const roomController = require("./controllers/room");
 const userController = require("./controllers/user");
 
-//map each controller to the app object
+
+const app = express();
+
+//parse application
+app.use(bodyParser.urlencoded({ extended: false }))
 
 
+app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=>{
-console.log(`Connected to MongoDB`);
-})
-.catch(err=>console.log(`Error occured when connected to db ${err}`));
-const PORT = process.env.PORT;
-app.listen(PORT,()=>{
-
-    console.log(`Web Server is up and running`);    
-});
-
-//This tells Express to set or register Handlebars as its' Template/View Engine
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-
-
-app.use("/",generalController);
-app.use("/",productController);
-app.use("/",featuredController);
-app.use("/room",roomController);
-app.use("/user",userController);
-app.use("/",(req,res)=>{
-    res.render("general/404");
-});
-
 
 
 //custom middleware functions
@@ -66,3 +40,39 @@ app.use((req,res,next)=>{
     }
     next();
 })
+
+
+app.use(fileUpload());
+
+// app.use(session({secret: `${process.env.SESSION_SECRET}` , resave: false,saveUninitialized: true}))
+
+// //custom middleware functions
+// app.use((req,res,next)=>{
+
+//     res.locals.user = req.session.user;
+//     next();
+// });
+
+
+
+app.use("/",generalController);
+app.use("/",productController);
+app.use("/",featuredController);
+app.use("/room",roomController);
+app.use("/user",userController);
+app.use("/",(req,res)=>{
+    res.render("general/404");
+});
+
+
+
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{
+console.log(`Connected to MongoDB`);
+})
+.catch(err=>console.log(`Error occured when connected to db ${err}`));
+const PORT = process.env.PORT;
+app.listen(PORT,()=>{
+
+    console.log(`Web Server is up and running`);    
+});
