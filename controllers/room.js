a0c35e108c93e6b1478f9536d33421d136da887b
@Inna <55448200+Inna-WD@users.noreply.github.com>
2020-04-10 22:23:21 -0400

    1004

--- controllers/room.js
+++ controllers/room.js
@@ -2,32 +2,43 @@
 /*********************room ROUTES***************************/
 const express = require('express')
 const router = express.Router();
+const roomModel = require("../models/room.js")
 
 
 //Route to direct use to Add Task form
-router.get("/add",(req,res)=>
-{
+router.get("/add", (req, res) => {
     res.render("Room/roomAddForm");
 });
 
 //Route to process user's request and data when the user submits the add room form
-router.post("/add",(req,res)=>
-{
+router.post("/add", (req, res) => {
 
-   
+    const newUser = {
+        title: req.body.title,
+        description: req.body.description,
+        dueDate: req.body.dueDate,
+        priority: req.body.priority
+    }
+
+    const room = new roomModel(newUser);
+    room.save()
+        .then(()=>{
+           res.redirect("/room/list") 
+        })
+        .catch(err => console.log(`Error inserting to the database: ${err} `));
+
 });
 
 ////Route to fetch all tasks
-router.get("/list",(req,res)=>
-{
+router.get("/list", (req, res) => {
     res.render("Room/roomDashboard");
-  
+
 });
 
 //Route to direct user to the task profile page
-router.get("/description",(req,res)=>{
+router.get("/description", (req, res) => {
 
-    
+
 
 })
 
@@ -42,4 +53,4 @@ router.get("/description",(req,res)=>{
 //router to delete user
 
 
-module.exports=router;
+module.exports = router;
